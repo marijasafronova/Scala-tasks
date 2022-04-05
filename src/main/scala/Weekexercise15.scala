a37820a55c5b47dd9ac91f463b8a402e543269a5
Marija <marijasafronova@gmail.com>
2022-04-05 21:10:13 +0300

    day exercise

---
+++ src/main/scala/Weekexercise15.scala
@@ -0,0 +1,20 @@
+object Weekexercise15 extends App{
+
+  def getDay(day: Int): String = day match {
+    case 1 => "monday"
+    case 2 => "tuesday"
+    case 3 => "wednesday"
+    case 4 => "thursday"
+    case 5 => "friday"
+    case 6 => "saturday"
+    case 7 => "sunday"
+    case _ => "unknown"
+  }
+
+  def getDayType(day: String): String = day match {
+    case "monday" | "tuesday" | "wednesday" | "thursday" | "friday"  => "weekday"
+    case "saturday" | "sunday" => "weekend"
+    case _ => "unknown"
+  }
+
+}
