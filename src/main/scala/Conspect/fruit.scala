a82d86dfb5861c2e4c7e50e38ce750ab1185434b
Marija <marijasafronova@gmail.com>
2022-04-26 21:09:28 +0300

    Json task

---
+++ src/main/scala/Conspect/fruit.scala
@@ -0,0 +1,13 @@
+package Conspect
+
+case class fruit(genus: String,
+                 name: String,
+                 id: Int,
+                 family: String,
+                 order: String,
+                 carbohydrates: Double=0,
+                 protein: Double=0,
+                 fat: Double=0,
+                 calories:Double=0,
+                 sugar:Double=0
+                )
