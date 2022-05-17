25acff6aa1f930dcc997defe72191d0b4106238c
Marija <marijasafronova@gmail.com>
2022-05-17 23:49:15 +0300

    added database: 3 tables and simple insertion for players table

--- src/main/scala/Final/Bulls_and_Cows.scala
+++ src/main/scala/Final/Bulls_and_Cows.scala
@@ -1,8 +1,12 @@
 package Final
 
 object Bulls_and_Cows extends App {
-  val BullsAndCowsGame = new BullsAndCows
+  //val BullsAndCowsGame = new BullsAndCows
 
-  BullsAndCowsGame.Play
+  //BullsAndCowsGame.Play
 
+  val db = new BullsAndCowsDatabase("src/resources/db/bullsandcows.db")
+  db.Migrate()
+  db.insertPlayer("Marija")
+  db.insertPlayer("Nikita")
 }
