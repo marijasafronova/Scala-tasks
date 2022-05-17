92eaeacc9f9f027361d26023d15c9c4e25c3f82b
Marija <marijasafronova@gmail.com>
2022-05-17 21:40:26 +0300

    refactoring of the game added: 2 game modes

--- src/main/scala/Final/BullsAndCows.scala
+++ src/main/scala/Final/BullsAndCows.scala
@@ -3,59 +3,113 @@ import scala.collection.mutable.ArrayBuffer
 import scala.io.StdIn.readLine
 import scala.util.Random
 
-class BullsAndCows(
-                    val playerA:String,
-                    val playerB:String,
-                    var isPlayerATurn:Boolean = true,
-                    var win: Boolean = false) {
-  println("Created a new game object of CowsAndBulls")
-  var currentState = true
-  var currentPlayer: String = if (isPlayerATurn) playerA else playerB
-  var guessesArray: ArrayBuffer[(String, String)] = ArrayBuffer()
+class BullsAndCows {
+  // What player turn
+  private var isPlayerATurn = true
+  // Game state
+  private var gameWon = false
 
-  def isSecondPlayerAComputer: Boolean = playerB == "COMPUTER"
+  // Storing players numbers for the game
+  private var numberA = List[Int]()
+  private var numberB = List[Int]()
 
-  def checkNumber(number: List[Int]): Boolean = {
-    val string = number.mkString
-    if (string.length == 4) {
-      string.length == string.distinct.length
+  // Storing player names for the game
+  private var playerA = ""
+  private var playerB = ""
+
+  // Guess history
+  private var guessesArray: ArrayBuffer[(String, String)] = ArrayBuffer()
+
+  private def isSecondPlayerAComputer: Boolean = playerB == "COMPUTER"
+
+  private def nextPlayer: Unit = isPlayerATurn = !isPlayerATurn
+
+  // Get history
+  private def getGuesses: Array[(String, String)] = guessesArray.toArray
+  // Get input in int list
+  private def getInputInList(msg: String): List[Int] = readLine(msg).trim.map(_.asDigit).toList
+  // Get current player
+  private def getCurrentPlayer: String = if (isPlayerATurn) playerA else playerB
+
+  private def getGuess(msg: String): List[Int] = {
+    var guess = getInputInList(msg)
+
+    while (!checkNumber(guess)) {
+      guess = getInputInList("Incorrect guess, enter again: ")
     }
-    else false
+
+    guess
   }
 
-  def randomizeNumber: List[Int] = {
+  private def getPlayerNumber: Unit = {
+    var input = List[Int]()
+
+    if (!isPlayerATurn && isSecondPlayerAComputer) {
+      input = randomizeNumber
+      println(s"Computer generated a number: ${input.mkString}")
+
+      numberB = input
+    }
+    else {
+      input = getInputInList(s"Player $getCurrentPlayer, enter your 4 digit number: ")
+
+      while (!checkNumber(input)) {
+        input = getInputInList("Your number must contain 4 unique digits, try again: ")
+      }
+
+      if (isPlayerATurn)
+        numberA = input
+      else
+        numberB = input
+    }
+  }
+
+  private def Greetings: Unit = {
+    playerA = readLine("Player A what is your name? ")
+    playerB = readLine("Player B what is your name? ")
+
+    println("\nGame begins!")
+    println(s"$playerA VS $playerB\n")
+  }
+
+  private def Menu: Unit = {
+    println("*" * 10 + " Bulls & Cows " + "*" * 10)
+    println("-" * 10 + "  Game Menu  " + "-" * 10)
+    println("1. Player vs Player")
+    println("2. Player vs Computer")
+  }
+
+  private def checkNumber(number: List[Int]): Boolean = {
+    val string = number.mkString
+    if (number(0) == 0) {
+      false
+    }
+    else if (string.length == 4) {
+      string.length == string.distinct.length
+    }
+    else
+      false
+  }
+
+   private def randomizeNumber: List[Int] = {
     var number = List[Int]()
     while (number.size < 4) { // player vs computer
-      val d = Random.nextInt(9) + 1
+      val d = Random.nextInt(10)
       if (!number.contains(d))
-        number = number :+ d
+        if (number.size == 0 && d != 0)
+          number = number :+ d
+        else if (number.size > 0)
+          number = number :+ d
     }
     number
   }
 
-  def getNumberB: List[Int] = {
-    var numB = List[Int]()
-    if (!isSecondPlayerAComputer) { //player vs player
-      numB = readLine(s"Player $playerB, enter your 4 digit number: ").trim.map(_.asDigit).toList
-      while (!checkNumber(numB)) {
-        numB = readLine("Your number must contain 4 unique digits, try again: ").trim.map(_.asDigit).toList
-        //in loop we are checking for correct input
-      }
-    }
-    else {
-      numB = randomizeNumber
-      println(s"Computer generated a number: ${numB.mkString}")
-    }
-    numB
-  }
-
-  def isGameActive: Boolean = currentState != win
-
-  def BullsCowsCounter(playerNum: List[Int], input: List[Int]): Unit = {
+  private def BullsCowsCounter(playerNum: List[Int], input: List[Int]): Unit = {
     var cows = 0
     var bulls = 0
-    if (isGameActive) {
-      for (i <- 0 to 3)
+
+    if (!gameWon) {
+      for (i <- 0 to 3) {
         if (playerNum(i) == input(i)) {
           bulls += 1
         }
@@ -63,44 +117,69 @@ class BullsAndCows(
           if (playerNum.contains(input(i)))
             cows += 1
         }
+      }
+
       if (bulls == 4) {
-        win = true
-        println(s"$currentPlayer you won, congratulations!")
-      } else {
-        println(s"Cows: $cows " +
-          s"Bulls: $bulls")
+        gameWon = true
+        println(s"$getCurrentPlayer you won, congratulations!")
+      }
+      else {
+        println( s"Bulls: $bulls Cows: $cows")
       }
     }
   }
 
-  def getGuesses: Array[(String, String)] = guessesArray.toArray
-
-  //FIX IT
-  def nextPlayer(): String = {
-    isPlayerATurn = !isPlayerATurn
-    currentPlayer = if (isPlayerATurn) playerA else playerB
-    currentPlayer
-  }
-
-//  def getLoser: String = {
-//    if (isGameActive) "N/A" // could be empty string
-//    else currentPlayer //since currentPlayer with no moves to make is the winner
-//  }
-//
-//  def getWinner: String = {
-//    if (isGameActive) "N/A" // could be empty string
-//    else { //game is finished
-//      if (isPlayerATurn) playerB else playerA
-//    }
-//  }
-
-
-    def printGuesses(): Array[(String, String)] = {
-      for (((guess, result), index) <- guessesArray.zipWithIndex) {
-        val playerName = if (index % 2 == 0) playerA else playerB
-        println(s"Guess ${index + 1}. $playerName guessed $guess resulting in $result.")
-      }
-      guessesArray.toArray
+  private def printGuesses(): Array[(String, String)] = {
+    for (((guess, result), index) <- guessesArray.zipWithIndex) {
+      val playerName = if (index % 2 == 0) playerA else playerB
+      println(s"Guess ${index + 1}. $playerName guessed $guess resulting in $result.")
     }
-
+    guessesArray.toArray
   }
+
+  private def PlayerVsPlayer: Unit = {
+    Greetings
+    getPlayerNumber
+    nextPlayer
+    getPlayerNumber
+    nextPlayer
+
+    while (!gameWon) {
+      if (isPlayerATurn) {
+        BullsCowsCounter(numberB, getGuess(s"Player $getCurrentPlayer, your guess: "))
+        nextPlayer
+      }
+      else {
+        BullsCowsCounter(numberA, getGuess(s"Player $getCurrentPlayer, your guess: "))
+        nextPlayer
+      }
+    }
+  }
+
+  private def PlayerVsComputer: Unit = {
+    playerA = readLine("Player, what is your name? ")
+    playerB = "COMPUTER"
+
+    println("\nGame begins!")
+    println(s"$playerA VS Computer\n")
+
+    nextPlayer
+    getPlayerNumber
+    nextPlayer
+
+    while (!gameWon) {
+      BullsCowsCounter(numberB, getGuess(s"Player $getCurrentPlayer, your guess: "))
+    }
+  }
+
+  def Play: Unit = {
+    Menu
+    val gameOption = readLine("Choose game mode: ").toInt
+
+    gameOption match {
+      case 1 => PlayerVsPlayer
+      case 2 => PlayerVsComputer
+      case _ => Play
+    }
+  }
+}
--- src/main/scala/Final/Bulls_and_Cows.scala
+++ src/main/scala/Final/Bulls_and_Cows.scala
@@ -1,42 +1,8 @@
 package Final
 
-import scala.io.StdIn.readLine
-
 object Bulls_and_Cows extends App {
+  val BullsAndCowsGame = new BullsAndCows
 
-  val playerA = readLine("Player A what is your name?")
-  var playerB = readLine("Player B what is your name? (press ENTER for computer) ")
-  if (playerB == "") playerB = "COMPUTER"
-
-  println(s"Player $playerA and Player $playerB let us play Bulls and Cows!")
-
-  var win = false // can put inside class
-  var isPlayerATurn=true // can put inside class
-  val BullsAndCowsGame = new BullsAndCows(playerA, playerB, isPlayerATurn, win)
-
-  var numberA = readLine(s"Player $playerA, enter your 4 digit number: ").trim.map(_.asDigit).toList
-  while(!BullsAndCowsGame.checkNumber(numberA)) {
-        numberA = readLine("Your number must contain 4 unique digits, try again: ").trim.map(_.asDigit).toList
-  }
-
-  var numberB = BullsAndCowsGame.getNumberB
-
-  while (BullsAndCowsGame.isGameActive) {
-    if (BullsAndCowsGame.currentPlayer==playerA) {
-      val input = readLine(s"Player $playerA, your guess: ").trim.map(_.asDigit).toList
-      BullsAndCowsGame.BullsCowsCounter(numberB, input)
-    } else  {
-      if (!BullsAndCowsGame.isSecondPlayerAComputer) {
-        val input = readLine(s"Player $playerB, your guess: ").trim.map(_.asDigit).toList
-        BullsAndCowsGame.BullsCowsCounter(numberA, input)
-      }
-      else {
-        val computerGuess = BullsAndCowsGame.randomizeNumber
-        println(s"Computer guess: ${computerGuess.mkString}")
-        BullsAndCowsGame.BullsCowsCounter(numberA, computerGuess)
-      }
-    }
-    BullsAndCowsGame.nextPlayer()
-  }
+  BullsAndCowsGame.Play
 
 }
