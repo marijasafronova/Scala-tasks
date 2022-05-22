54586f4215049b025ad745074ea6783a676aafc7
Marija <marijasafronova@gmail.com>
2022-05-22 16:43:11 +0300

    Final

--- src/main/scala/Tasks/Text_tools.scala
+++ src/main/scala/Tasks/Text_tools.scala
@@ -30,11 +30,22 @@ object Text_tools {
     saveText(dst, text)
     text //we return the text just in case we want to save and do some work as well
   }
-
+  def getTextFromFile(src: String):String = {
+    val bufferedSource = Source.fromFile(src) //think of bufferedSource as a stream of bytes
+    val text = bufferedSource.mkString //we convert this stream into actual string
+    bufferedSource.close() //important to close the file
+    text
+  }
   def getWordCountPerLine(lines: Array[String], sep: String = " +"): Array[Int] = {
     val wordsLines = lines.map(_.split(sep)) //so we get an Array of Array of words
     val wordsPerLine = wordsLines.map(_.length)
     wordsPerLine
+  }
+  def getLinesFromFile(src: String):Array[String] = {
+    val bufferedSource = Source.fromFile(src)
+    val lines = bufferedSource.getLines().toArray
+    bufferedSource.close()
+    lines
   }
 
   def getListOfFiles(dir: String, regex: String=".*"): List[File] ={
@@ -44,6 +55,17 @@ object Text_tools {
     } else {
       List[File]()
     }
+  }
+  def myRound(n: Double, precision: Int = 0): Double = {
+    //so the trick would be to multiply by some power of 10 then divide by that mulitplier
+    //so (n*100).round/100 would give us 2 points of precision
+    //why because built in round gives us 0 precision
+    //so if we have 100 that is 10 to the 2nd power(squared)
+    //we can use the built in Math.pow to do that
+    val multiplier = Math.pow(10, precision) //so precision 0 will give us 10 to 0 which is 1
+
+    //    n.round //only 0 precision
+    (n * multiplier).round / multiplier //we utilize the built in round
   }
 
 }
